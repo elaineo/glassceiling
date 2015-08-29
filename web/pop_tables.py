093ffeccb0eb328139ad8475fb963cd6ba7b56b7
Elaine Ou <elaine.ou@gmail.com>
2015-08-28 22:15:12 -0700

    more data

--- web/pop_tables.py
+++ web/pop_tables.py
@@ -9,3 +9,12 @@ def populate_linkedin(joblist=None):
             cur.execute("INSERT INTO linkedin (company, title) VALUES (%s,%s)" % 
                 (j['company'], j['position']))
     con.close()
+
+def populate_glassdoor(joblist=None):
+    conn = db.connect('localhost', 'root', '', 'glass')
+    with conn:
+        cur = conn.cursor()
+        for j in joblist:
+            cur.execute("INSERT INTO glassdoor (company,title,count,max_sal,min_sal,mean_sal) VALUES (%s,%s)" % 
+                (j['company'], j['position']))
+    con.close()
